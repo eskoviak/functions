bc7659d90f3c6dc0a8caf7faecac0812166f81b9
Ed Skoviak <eskoviak@gmail.com>
2025-07-05 13:35:18 -0500

    Following along tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,7 @@
-use postgres::{Client, NoTls, Error};
-use env::var;
+fn main() {
+    another_function(5);
+}
 
-fn main() -> Result<(), Error> {
-
-    let pg_url = env:var("PG_LOCAL").unwrap_or_else(|_| "postgresql://postgres:postgres@localhost/library".to_string());
-
-    println!("Connecting to PostgreSQL at: {}", pg_url);
-
-    /*
-    let mut client = Client::connect(&pg_url, NoTls)?;
-    
-    client.batch_execute("
-        CREATE TABLE IF NOT EXISTS author (
-            id              SERIAL PRIMARY KEY,
-            name            VARCHAR NOT NULL,
-            country         VARCHAR NOT NULL
-            )
-    ")?;
-
-    client.batch_execute("
-        CREATE TABLE IF NOT EXISTS book  (
-            id              SERIAL PRIMARY KEY,
-            title           VARCHAR NOT NULL,
-            author_id       INTEGER NOT NULL REFERENCES author
-            )
-    ")?;
-    */
-    Ok(())
-
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
 }
